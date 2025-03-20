d089c5ebab05e7e66518ca2b5cd91e849468312b
student <111gagara111@gmail.com>
2025-03-20 21:48:27 +0300

    4

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    context =  {"title": "Главная страница"}
+    return render(request,template_name='blog/index.html', context=context)
+
+def about(request):
+    return HttpResponse('<h1>О Сайте!</h1>')
