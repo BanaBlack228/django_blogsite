5e20d3d1be36b44188c4084aa36dff5283b435d2
student <111gagara111@gmail.com>
2025-03-27 21:45:20 +0300

    5

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#Описание модели поста
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Загаловок")
+    text = models.TextField(verbose_name="Текст поста")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания", editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+
+
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title
+        # Create your models here.
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import index, about
+from blog.views import index, about, add_post
 
 app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', add_post, name='add_post'),
     path('about/', about, name='about')
 ]
