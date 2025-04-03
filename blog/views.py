3c681267bffc16597af6160bf24cbae459e8dfe2
student <111gagara111@gmail.com>
2025-04-03 21:42:19 +0300

    8

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from lib2to3.fixes.fix_input import context
 
 from django.db.models.signals import post_init
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
@@ -22,12 +22,13 @@ def add_post(request):
         return render(request, template_name='blog/post_add.html', context=context)
 
     if request.method == 'POST':
-        post_form = PostForm(data=request.POST)
+        post_form = PostForm(data=request.POST, files=request.FILES)
         if post_form.is_valid():
             post = Post()
             post.title = post_form.cleaned_data['title']
             post.text = post_form.cleaned_data['text']
             post.author = post_form.cleaned_data['author']
+            post.image = post_form.cleaned_data['image']
             post.save()
             return index(request)
 
@@ -35,3 +36,23 @@ def read_post(request, pk):
     post = Post.objects.get(pk=pk)
     context = {"title":"Информация о посте","post": post}
     return render(request, template_name="blog/post_detail.html", context=context)
+
+def update_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST, files=request.FILES)
+        if post_form.is_valid():
+            post.title = post_form.cleaned_data['title']
+            post.text = post_form.cleaned_data['text']
+            post.author = post_form.cleaned_data['author']
+            post.image = post_form.cleaned_data['image']
+            post.save()
+            return redirect('blog:read_post', pk=post.id)
+    else:
+        post_form = PostForm(initial={
+        "title": post.title,
+        "author": post.author,
+        "text": post.text,
+        "image": post.image
+        })
+        return render(request, template_name="blog/post_edit.html", context={"form": post_form})
